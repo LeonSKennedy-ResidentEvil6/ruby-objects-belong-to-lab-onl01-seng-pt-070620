4747efad710925ffc2363decc9275075bd5a5af0
Ebo Lee <yiboli.coding@gmail.com>
2020-07-30 17:30:46 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,11 @@
+class Artist
+  
+  attr_accessor :name, :title
+  
+  def initialize(title,name)
+    @name = name
+    @title = title
+  end 
+  
+end 
+
---
+++ lib/song.rb
@@ -0,0 +1,14 @@
+class Song 
+  
+  attr_accessor :artist, :title 
+  
+  def initialize(artist, title)
+    @artist = artist
+    @title = title
+  end 
+  
+end 
+
+  artist = Artist.new
+  song.artist = artist
+  song.artist.name 
